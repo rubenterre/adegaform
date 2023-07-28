5c5873f533fe56f60da49dead0f68d93b77a2e3e
Rubén Terré <72130385+rubenterre@users.noreply.github.com>
2023-07-28 23:22:28 +0200

    Mod function print PDF

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,25 +4,70 @@
 
 	export let data;
 
+	let codigoadega = '';
+	let asuntorelacionado = '';
+	let deteccion = '';
+	let poligono = '';
+	let parcela = '';
+	let concello = '';
+	let provincia = '';
+	let seprona = '';
+	let policia_autonomica = '';
+	let conselleria = '';
+	let ministerio = '';
 
+	function imprimir() {
+    // Crea el contenido que deseas imprimir, por ejemplo, utilizando un formato de cadena
+    const contenidoParaImprimir = `
+	<p>Código de ADEGA: DF ${codigoadega}</p>
+    <p>Asunto relacionado: ${asuntorelacionado}</p>
+	<p>Recentemente, membros de ADEGA detectaron: ${deteccion}</p>
+	<br>
+	<h4>DATOS DE UBICACIÓN</h4>
+	<p>Poligono: ${poligono}</p>
+	<p>Parcela: ${parcela}</p>
+	<p>Concello: ${concello}</p>
+	<p>Provincia: ${provincia}</p>
+	<br>
+	<h4>LEIS INCUMPRIDAS</h4>
+    `;
+
+    // Crea un elemento de iframe oculto para imprimir el contenido
+    const iframe = document.createElement('iframe');
+    iframe.style.display = 'none';
+    document.body.appendChild(iframe);
+
+    // Abre el iframe y carga el contenido
+    const iframeWindow = iframe.contentWindow;
+    iframeWindow.document.open();
+    iframeWindow.document.write(contenidoParaImprimir);
+    iframeWindow.document.close();
+
+    // Envía el contenido del iframe a la impresora
+    iframeWindow.print();
+
+    // Remueve el iframe después de la impresión
+    iframe.remove();
+  }
 </script>
 
 <Header />
 
 <section class="formulario_inicial">
 	<div class="container">
-		<form id="DenunciaAmbiental">
+		<form id="DenunciaAmbiental" onsubmit="event.preventDefault(); result();">
 			<div class="row">
 				<div class="input-field outlined col s12 m6 valign-wrapper formulario">
-					<input id="codigoadega" type="text" class="validate" />
+					<input id="codigoadega" type="text" class="validate" bind:value={codigoadega}/>
 					<label for="codigoadega">Código de ADEGA: DF</label>
 				</div>
 				<div class="input-field outlined col s12 m6 valign-wrapper formulario">
-					<input id="asuntorelacionado" type="text" class="validate" />
+					<input id="asuntorelacionado" type="text" class="validate" bind:value={asuntorelacionado}/>
 					<label for="asuntorelacionado">Asunto relacionado:</label>
 				</div>
 				<div class="input-field outlined col s12 formulario">
 					<textarea
+					bind:value={deteccion}
 						id="deteccion"
 						class="materialize-textarea"
 						placeholder="Incluír a data aproximada, unha descrición dos feitos denunciados, e indicar a referencia catastral e/ou cordeadas X/Y se as houbese."
@@ -66,19 +111,19 @@
 						<div class="ubicacion-datos">
 							<h5>DATOS DE UBICACIÓN</h5>
 							<div class="input-field outlined col s12 m6 valign-wrapper formulario">
-								<input id="poligono" type="text" class="validate" />
+								<input id="poligono" type="text" class="validate" bind:value={poligono}/>
 								<label for="poligono">Polígono</label>
 							</div>
 							<div class="input-field outlined col s12 m6 valign-wrapper formulario">
-								<input id="parcela" type="text" class="validate" />
+								<input id="parcela" type="text" class="validate" bind:value={parcela}/>
 								<label for="parcela">Parcela</label>
 							</div>
 							<div class="input-field outlined col s12 m6 valign-wrapper formulario">
-								<input id="concello" type="text" class="validate" />
+								<input id="concello" type="text" class="validate" bind:value={concello}/>
 								<label for="concello">Concello</label>
 							</div>
 							<div class="input-field outlined col s12 m6 valign-wrapper formulario">
-								<input id="provincia" type="text" class="validate" />
+								<input id="provincia" type="text" class="validate" bind:value={provincia}/>
 								<label for="provincia">Provincia</label>
 							</div>
 						</div>
@@ -89,7 +134,7 @@
 							{#each data.articulos as articulo}
 								<p class="formulario">
 									<label>
-										<input type="checkbox" id={articulo.id} />
+										<input type="checkbox" id={articulo.id} bind:value={articulo.id}/>
 										<span><b>{articulo.articulo}</b> {articulo.contenido}</span>
 									</label>
 								</p>
@@ -101,7 +146,7 @@
 							{#each data.leis as lei}
 								<p class="formulario">
 									<label>
-										<input type="checkbox" id={lei.id} />
+										<input type="checkbox" id={lei.id} bind:value={lei.id}/>
 										<span><b>{lei.articulo}</b> {lei.contenido}</span>
 									</label>
 								</p>
@@ -114,19 +159,19 @@
 							<h5>ELIXE DESTINATARIO</h5>
 							<p class="formulario">
 								<label>
-									<input name="seprona" type="radio" />
+									<input id="seprona" name="seprona" type="radio" bind:value={seprona}/>
 									<span>SERVIZO DE PROTECCIÓN DA NATUREZA (SEPRONA)</span>
 								</label>
 							</p>
 							<p class="formulario">
 								<label>
-									<input name="policia_autonomica" type="radio" />
+									<input id="policia_autonomica" name="policia_autonomica" type="radio" bind:value={policia_autonomica}/>
 									<span>POLICÍA AUTONÓMICA</span>
 								</label>
 							</p>
 							<p class="formulario">
 								<label>
-									<input name="ministerio" type="radio" />
+									<input id="ministerio" name="ministerio" type="radio" bind:value={ministerio} />
 									<span
 										>MINISTERIO PARA A TRANSICIÓN ECOLÓXICA E O RETO DEMOGRÁFICO,CONFEDERACIÓN
 										HIDROGRÁFICA MIÑO SIL</span
@@ -135,7 +180,7 @@
 							</p>
 							<p class="formulario">
 								<label>
-									<input name="conselleria" type="radio" />
+									<input id="conselleria" name="conselleria" type="radio" bind:value={conselleria}/>
 									<span>CONSELLERÍA DE INFRAESTRUCTURAS E MOBILIDADE- AUGAS DE GALICIA</span>
 								</label>
 							</p>
@@ -208,7 +253,7 @@
 						</p>
 					</div>
 					<div class="imprimir">
-						<a class="white-text waves-effect waves-light btn" onclick= ""
+						<a class="white-text waves-effect waves-light btn" type="button" on:click={imprimir}
 							>IMPRIMIR DOCUMENTO</a
 						>
 					</div>
